a5335356aefa2faf09790b866376c264b4c18e8a
shilinlee <836160610@qq.com>
2024-04-04 11:37:33 +0800

    docs: init blog template using hope-theme

---
+++ src/.vuepress/config.ts
@@ -0,0 +1,15 @@
+import { defineUserConfig } from "vuepress";
+import theme from "./theme.js";
+
+export default defineUserConfig({
+  base: "/",
+
+  lang: "zh-CN",
+  title: "博客演示",
+  description: "vuepress-theme-hope 的博客演示",
+
+  theme,
+
+  // 和 PWA 一起启用
+  // shouldPrefetch: false,
+});
